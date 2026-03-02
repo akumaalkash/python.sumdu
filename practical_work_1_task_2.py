4a77afda593193f3df799de1b55c051a6acbfcd7
akumaalkash <sashaskzy@gmail.com>
2026-03-02 11:33:54 +0100

    practical_work_1_task_2_3

---
+++ practical_work_1_task_2.py
@@ -0,0 +1,3 @@
+for n in range(1, 11):
+    result = 2 ** n
+    print('2 в степені ', n, '=', result, end='.')
---
+++ practical_work_1_task_3.py
@@ -0,0 +1,6 @@
+N = int(input("Enter number N: "))
+
+for i in range(N, 0, -1):   
+    print((str(N) + " ") * i)
+for i in range(1, N + 1):
+    print((str(N) + " ") * i) 
